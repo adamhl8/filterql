f0644d34abe2896b4a1a28c1af9112593c2b9500
Adam Langbert <adamhl@pm.me>
2025-09-16 10:12:16 -0500

    bump deps

--- knip.ts
+++ knip.ts
@@ -1,8 +1,7 @@
 import { knipConfig } from "@adamhl8/configs"
-import type { KnipConfig } from "knip"
 
 const config = knipConfig({
   project: ["!src/test-setup.ts"],
-} as const) satisfies KnipConfig
+} as const)
 
 export default config
